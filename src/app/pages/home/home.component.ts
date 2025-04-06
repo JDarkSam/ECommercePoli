9f2cd86f69360ea291921721804ffb7b4f546d86
Sam <jayi.darksam@gmail.com>
2025-04-06 17:15:41 -0500

    se organiza componente home y admin productos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,9 +9,9 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent {
   products = [
-    { name: 'Altavoz JBL Flip 5', price: '$350.000', image: 'assets/jbl.jpg' },
-    { name: 'SSD Sandisk 1TB', price: '$465.000', image: 'assets/sandisk.jpg' },
-    { name: 'iPad Pro M4 256GB', price: '$4.299.900', image: 'assets/ipad.jpg' }
+    { name: 'Altavoz JBL Flip 5', price: '$350.000', image: 'jbl_flip5.png' },
+    { name: 'SSD Sandisk 1TB', price: '$465.000', image: 'disco_ssd.png' },
+    { name: 'iPad Pro M4 256GB', price: '$4.299.900', image: 'ipad_pro.png' }
   ];
 
   constructor(private router: Router) {}
